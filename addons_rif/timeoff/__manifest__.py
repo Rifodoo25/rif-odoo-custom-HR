12b83f2a0ebeb0d331138528bbd6c0a1229ef908
narjessbennejma <narjessbennejma02@gmail.com>
2025-08-06 21:05:57 +0200

    Add initial version of TimeOff module for employee leave management

---
+++ addons_rif/timeoff/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "TimeOff",
+    "summary": "Gestion des congés des employés",
+    "version": "18.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Human Resources",
+    "author": "Ton Nom ou Groupe",
+    "website": "https://tonsite.com",
+    "depends": ["base","hr","hr_holidays","mail","portal","calendar"],
+    "data": [],
+    'installable': True,
+    'auto_install': False,
+    'application':True,
+}
