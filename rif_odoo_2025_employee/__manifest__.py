0a9a225f01502ca2a481c0179020b0d453573821
abdelhamid2506 <mohamedabdelhamidfarhat@gmail.com>
2025-08-07 21:01:45 +0100

    employee part with departement and contrat

---
+++ rif_odoo_2025_employee/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "rif_odoo_2025_employee_part_2025",
+    "summary": "Building and testing rif_odoo_employee_part_2025 to be added in the official project",
+    "version": "1.0",
+    "license": "LGPL-3",
+    "depends": [
+        #orm
+        "base",
+        #employee
+        "hr",
+        #contrat
+        "hr_contract",
+        #equipements
+        "maintenance",
+        #skills and CV
+        "hr_skills",
+        "hr_skills_slides",
+        "hr_skills_survey",
+        
+    ],
+    "data": [
+        
+    ],
+    "demo": [],
+    "assets": {}
+}
