a0e4f8d4d331bf6800a9f9775338ae870b947d45
abdelhamid2506 <mohamedabdelhamidfarhat@gmail.com>
2025-08-08 18:33:43 +0100

    employee+ presence + todo list

--- rif_odoo_2025_employee/__manifest__.py
+++ rif_odoo_2025_employee/__manifest__.py
@@ -9,13 +9,19 @@
         #employee
         "hr",
         #contrat
-        "hr_contract",
+        "hr_contract", 
+            # Optional, for digital signatures
+        "sign",  
         #equipements
         "maintenance",
         #skills and CV
         "hr_skills",
         "hr_skills_slides",
         "hr_skills_survey",
+        # hr_attendance is the base presence module
+        'hr_attendance',
+        #to do list
+        "hr_timesheet",
         
     ],
     "data": [
